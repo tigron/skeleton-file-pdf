f79ee7275d08b043a6038e9f8c22ee0c4d3442bf
David Vandemaele <david@tigron.be>
2016-06-14 14:17:43 +0200

    Add config

---
+++ lib/Skeleton/File/Pdf/Config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Config class
+ * Configuration for Skeleton\File\Pdf
+ *
+ * @author David Vandemaele <david@tigron.be>
+ */
+
+namespace Skeleton\File\Pdf;
+
+class Config {
+
+	/**
+	 * Pdf interface class
+	 *
+	 * This class will provide the Pdf functionality, by default a class is defined
+	 */
+	public static $pdf_interface = '\Skeleton\File\Pdf\Pdf';
+}
